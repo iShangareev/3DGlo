c0cea28b0c992fe13f09106e61a99a77792b9fc5
Ильнар Шангареев <i.shangareyev@modulbank.ru>
2022-03-07 15:12:09 +0400

    lesson024

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import validator from './modules/validator';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
 
 timer('30 march 2022');
@@ -12,3 +13,4 @@ modal()
 validator()
 tabs()
 slider()
+calc()
