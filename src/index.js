7573d1969516dfcf333f1e5d03527158ad255291
Ильнар Шангареев <i.shangareyev@modulbank.ru>
2022-03-15 14:35:58 +0400

    lesson027

--- src/index.js
+++ src/index.js
@@ -25,8 +25,20 @@ sendForm({
   ]
 })
 sendForm({
-  formId: 'form2'
+  formId: 'form2',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total'
+    }
+  ]
 })
 sendForm({
-  formId: 'form3'
+  formId: 'form3',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total'
+    }
+  ]
 })
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,6 +4,7 @@ const menu = () => {
 
 
   body.addEventListener('click', (e) => {
+    e.preventDefault()
     if(e.target.closest('menu>ul>li>a') || e.target.closest('.menu') || e.target.closest('.close-btn')) {
       menu.classList.toggle('active-menu')
     }
